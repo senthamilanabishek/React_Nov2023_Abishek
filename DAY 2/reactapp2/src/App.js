ae95840dcefb4ca89cbd09e2603d57e5564bdbb7
Abishek.T <124855977+senthamilanabishek@users.noreply.github.com>
2023-11-16 12:44:10 +0530

    Add files via upload

---
+++ DAY 2/reactapp2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Styling from './components/Styling';
+
+function App() {
+  return (
+    <div className="App">
+      <Styling/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ DAY 2/reactapp3/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Aboutme from "./components/Aboutme";
+
+function App(){
+    return (
+        <div className="App">
+            <Aboutme/>
+        </div>
+    )
+}
+    
+
+
+export default App;
